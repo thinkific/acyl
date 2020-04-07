98a919fa07dd2f58c0e4237fed19d073ecbfe140
esterlinej <joel.esterline@gmail.com>
2020-04-07 14:34:33 -0700

    Code Review Updates - Remove deprecated mocks package - Remove spawner/aliases.go, update spawner and fake accordingly

--- pkg/spawner/aliases.go
+++
@@ -1,9 +0,0 @@
-package spawner
-
-import (
-  "github.com/dollarshaveclub/acyl/pkg/models"
-)
-
-type QAEnvironment = models.QAEnvironment
-type RepoRevisionData = models.RepoRevisionData
-type QADestroyReason = models.QADestroyReason
--- pkg/spawner/spawner.go
+++ pkg/spawner/spawner.go
@@ -1,15 +1,16 @@
 package spawner
 
 import (
+	"github.com/dollarshaveclub/acyl/pkg/models"
 	"golang.org/x/net/context"
 )
 
 // EnvironmentSpawner describes an object capable of managing environments
 type EnvironmentSpawner interface {
-	Create(context.Context, RepoRevisionData) (string, error)
-	Update(context.Context, RepoRevisionData) (string, error)
-	Destroy(context.Context, RepoRevisionData, QADestroyReason) error
-	DestroyExplicitly(context.Context, *QAEnvironment, QADestroyReason) error
+	Create(context.Context, models.RepoRevisionData) (string, error)
+	Update(context.Context, models.RepoRevisionData) (string, error)
+	Destroy(context.Context, models.RepoRevisionData, models.QADestroyReason) error
+	DestroyExplicitly(context.Context, *models.QAEnvironment, models.QADestroyReason) error
 	Success(context.Context, string) error
 	Failure(context.Context, string, string) error
 }
